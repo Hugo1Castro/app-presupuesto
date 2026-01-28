aacf03c899f2aa99e384f089632b5a6825180657
Hugo Castro <javcastro@live.cl>
2026-01-28 19:36:19 -0300

    feat: 59 Implementacion de lista de ingresos con pipes, control flow y logica de eliminacion en servicio

--- src/app/ingreso/ingreso.service.ts
+++ src/app/ingreso/ingreso.service.ts
@@ -10,5 +10,12 @@ export class IngresoService {
   new Ingreso('Salario', 4000),
   new Ingreso('Venta Coche', 500)
 
- ]
+ ];
+
+ eliminar(ingreso: Ingreso){
+  const indice: number = this.ingresos.indexOf(ingreso);
+  if (indice !== -1) { // ¡Solo borra si realmente lo encontró!
+  this.ingresos.splice(indice, 1);
+ }
+}
 }
