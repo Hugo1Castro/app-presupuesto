a528cdffae935036bb322fc9849a2df50913c1e7
Hugo Castro <javcastro@live.cl>
2026-01-28 02:30:25 -0300

    feat: definir modelos y servicios de ingreso y egreso con datos iniciales

--- src/app/egreso/egreso.service.ts
+++ src/app/egreso/egreso.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Egreso } from './egreso.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EgresoService {
+  egresos: Egreso[] = [
+    new Egreso('Renta Depto', 900),
+    new Egreso('Ropa', 500)
 
-  constructor() { }
+  ]
 }
+
