6d58135610dce104a51fa3a79e33eff000cdf484
Hugo Castro <javcastro@live.cl>
2026-01-28 23:45:45 -0300

    feat: 60 Implementacion de lista de egresos, calculo de porcentajes e inyeccion de servicio

--- src/app/egreso/egreso.component.ts
+++ src/app/egreso/egreso.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Egreso } from './egreso.model';
+import { EgresoService } from './egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-egreso',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './egreso.component.html',
   styleUrl: './egreso.component.css'
 })
 export class EgresoComponent {
+  egresos: Egreso[] =[];
+
+  @Input() ingresoTotal! :number;
+
+  constructor(private egresoServicio: EgresoService){
+    this.egresos = this.egresoServicio.egresos;
+  }
+
+  eliminarEgreso(egreso: Egreso){
+    this.egresoServicio.eliminar(egreso);
+  }
+
+  calcularPorcentaje(egreso: Egreso){
+    if(this.ingresoTotal > 0){
+      return egreso.valor/this.ingresoTotal;}
+    return 0;
+    }
+    
+  
 
 }
+
+
--- src/app/egreso/egreso.service.ts
+++ src/app/egreso/egreso.service.ts
@@ -9,7 +9,12 @@ export class EgresoService {
   egresos: Egreso[] = [
     new Egreso('Renta Depto', 900),
     new Egreso('Ropa', 200)
-
-  ]
+  ];
+  eliminar(egreso: Egreso){
+    const indice: number = this.egresos.indexOf(egreso);
+    // if(indice! == -1){}
+    this.egresos.splice(indice, 1);
+  
+  }
 }
 
