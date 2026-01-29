6fd7cd1b0d4e348ec6eed0dbc4d5490a8b76fab6
Hugo Castro <javcastro@live.cl>
2026-01-29 13:15:10 -0300

    feat: implementacion de formulario con two-way binding y validacion de envio

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,18 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IngresoService } from '../ingreso/ingreso.service';
+import { EgresoService } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { Egreso } from '../egreso/egreso.model';
 
 @Component({
   selector: 'app-formulario',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
   tipo: string = "ingresoOperacion";
 
+  //definimos Input del formulario
+descripcionInput: string | null = null;
+valorInput: number | null = null;
+
+constructor(private ingresoServicio: IngresoService, private egresoServicio: EgresoService) {}
+
   tipoOperacion(event: Event){
     const elementoSelect = event.target as HTMLSelectElement;
     this.tipo = elementoSelect.value;
+  }
+  agregarValor(){
+    console.log("El tipo seleccionado es: ", this.tipo); // <-- ESPÍA 1
+    if(this.descripcionInput != null && this.valorInput != null){
+      if(this.tipo == 'ingresoOperacion'){
+        this.ingresoServicio.ingresos.push(
+          new Ingreso(this.descripcionInput, this.valorInput)
+        )
+      }
+      else{
+        console.log("Entró a la lógica de EGRESO porque el IF falló"); // <-- ESPÍA 2
+        this.egresoServicio.egresos.push(
+        new Egreso(this.descripcionInput, this.valorInput)
+      )
+
+      }
+    }
+    else{
+      console.log('Introduce valores en descripcion y valor válidos ')
+    }
   }
 
 }
